04784eaffeaa78fc6b673d8a70667961ddf0ddb8
Himanshu Tiwari <tiwarih81@gmail.com>
2022-07-24 18:29:15 +0530

    before CRUD

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -4,3 +4,6 @@ class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     
+    def __str__(self):
+        return self.name + " ," + self.description 
+    
---
+++ drinks/drinks/serializers.py
@@ -0,0 +1,10 @@
+# this will link jason form
+
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ["id", "name", "description"]
+    
